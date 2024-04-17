e042343ce7a23f5d9c13350d16a2955e9b74cf7e
ICawesome <162729625+ICawesome@users.noreply.github.com>
2024-04-17 15:52:20 +0000

    RGB

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+r = int(input("enter the red value: "))
+g = int(input("enter the green value: "))
+b = int(input("enter the blue value: "))
+if r > 255:
+    print("the red value is not in the acceptable range")
+if g > 255:
+    print("the green value is not in the acceptable range")
+if b > 255:
+    print("the blue value is not in the acceptable range")
+if r <= 255 and g <= 255 and b <= 255:
+    print("no problems found")
